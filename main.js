341ce886ba5635f6764fc8f08c7e8acb9fbe1b6b
Taylor Ivanoff <taylorivanoff@gmail.com>
2026-01-27 21:01:54 +0100

    Remove quick actions from tray menu and jump list

--- main.js
+++ main.js
@@ -134,8 +134,6 @@ function createTray() {
   tray.setContextMenu(Menu.buildFromTemplate([
     { label: `Show ${APP_NAME}`, click: () => { if (!mainWindow) createWindow(); mainWindow.show(); } },
     { type: 'separator' },
-    { label: 'New Reminder', click: () => { if (!mainWindow) createWindow(); mainWindow.show(); mainWindow.webContents.executeJavaScript('document.querySelector("[data-click=newreminder]")?.click()'); } },
-    { type: 'separator' },
     { label: 'Check for Updates', click: () => autoUpdater.checkForUpdatesAndNotify() },
     { label: 'Quit', click: () => { isQuitting = true; app.quit(); } }
   ]));
@@ -143,11 +141,7 @@ function createTray() {
 }
 
 function setupJumpList() {
-  app.setJumpList([
-    { type: 'tasks', items: [
-      { type: 'task', title: 'New Reminder', description: 'Create a new reminder', program: process.execPath, args: `${PROTOCOL}://new`, iconPath: process.execPath, iconIndex: 0 }
-    ]}
-  ]);
+  app.setJumpList([]);
 }
 
 function setupAutoUpdater() {
